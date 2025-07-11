edbf1a8646280d9dd3de8b96d479ecdf333d68a2
Muhammad Vikri <muhammadvikrii99@gmail.com>
2025-07-11 09:35:41 +0700

    fix: delete unused import

--- app/src/main/java/com/elearn/presentation/ui/components/ActionBar.kt
+++ app/src/main/java/com/elearn/presentation/ui/components/ActionBar.kt
@@ -1,9 +1,7 @@
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Icon
@@ -13,7 +11,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
